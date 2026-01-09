88e9a33b8b2f0e16383300a9eb3d7c6cac91fa02
DareenKN <dkinganjatou1@nait.ca>
2026-01-09 15:48:57 -0700

    Moved stuff over for a better landing page

--- demos/week11/week11/le11Demo1/week11_1.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "Hi- It's a nice day today"."<br>";
-echo "name=".$_GET['getName']."<br>";
-echo "score=".$_GET['getScore'];
-?>                        
--- demos/week11/week11/le11Exercise2/js/le11Ex2.js
+++
@@ -1,7 +0,0 @@
-/*Arrow function loaded when the html document is ready. 
-   Note that this is similar to the event handler we 
-   have for the window.onload event.
-   */
-$(()=>{
-    
-})
