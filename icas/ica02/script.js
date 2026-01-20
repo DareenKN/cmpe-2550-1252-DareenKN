8a193dae05e191fb6db302b57007120d07afad8b
DareenKN <dkinganjatou1@nait.ca>
2026-01-19 21:57:44 -0700

    Fix

--- icas/ica02/script.js
+++ icas/ica02/script.js
@@ -5,24 +5,35 @@ document.addEventListener("DOMContentLoaded", () => {
         body: JSON.stringify({ action: "init" })
     })
     .then(r => r.json())
-    .then(d => updateBoard(d.board));
+    .then(d => {
+        if (d.board) {
+            updateBoard(d.board);
+        }
+    });
 
     document.querySelectorAll(".cell").forEach(cell => {
         cell.onclick = () => {
             const [r, c] = cell.id.split("_");
+
             fetch("gameFlow.php", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ action: "move", row: r, col: c })
             })
             .then(r => r.json())
-            .then(d => updateBoard(d.board));
+            .then(d => {
+                if (d.board) {
+                    updateBoard(d.board);
+                }
+            });
         };
     });
 });
 
 function updateBoard(board) {
-    for (let r = 0; r < 3; r++)
-        for (let c = 0; c < 3; c++)
+    for (let r = 0; r < 3; r++) {
+        for (let c = 0; c < 3; c++) {
             document.getElementById(`${r}_${c}`).value = board[r][c];
+        }
+    }
 }
