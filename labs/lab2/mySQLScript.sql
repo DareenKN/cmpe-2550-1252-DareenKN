b698305d12f01fed3f95b5dc1c61d43859b37a60
DareenKN <dkinganjatou1@nait.ca>
2026-03-13 18:56:57 -0600

    lab 2

--- labs/lab2/mySQLScript.sql
+++ labs/lab2/mySQLScript.sql
@@ -18,8 +18,7 @@ DROP TABLE IF EXISTS user_roles;
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    password_hash VARCHAR(255) NOT NULL
 );
 
 -- ROLES TABLE
