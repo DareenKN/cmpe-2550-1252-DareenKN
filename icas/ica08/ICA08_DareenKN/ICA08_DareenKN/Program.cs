6bd79fd4a530dd975baaafd0630429ceabe6e90f
DareenKN <dkinganjatou1@nait.ca>
2026-03-20 20:22:22 -0600

    Completed

---
+++ icas/ica08/ICA08_DareenKN/ICA08_DareenKN/Program.cs
@@ -0,0 +1,15 @@
+namespace ICA08_DareenKN
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
---
+++ icas/ica08/query.sql
@@ -0,0 +1,11 @@
+use dkinganjatou1_pubs
+go
+
+select *
+from authors
+
+select distinct type
+from titles
+
+select * from authors
+ORDER BY au_lname;
