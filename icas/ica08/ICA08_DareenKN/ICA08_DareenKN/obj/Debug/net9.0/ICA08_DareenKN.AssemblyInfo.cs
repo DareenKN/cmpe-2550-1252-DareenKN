c8a044f3c3df62ddba2e348ce34d730d69eb9679
DareenKN <dkinganjatou1@nait.ca>
2026-03-20 20:26:38 -0600

    Started

--- icas/ica08/ICA08_DareenKN/ICA08_DareenKN/obj/Debug/net9.0/ICA08_DareenKN.AssemblyInfo.cs
+++ icas/ica08/ICA08_DareenKN/ICA08_DareenKN/obj/Debug/net9.0/ICA08_DareenKN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ICA08_DareenKN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62dba9cc17907de3f1c7be2d38d7e9000a01d169")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bd79fd4a530dd975baaafd0630429ceabe6e90f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ICA08_DareenKN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ICA08_DareenKN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
