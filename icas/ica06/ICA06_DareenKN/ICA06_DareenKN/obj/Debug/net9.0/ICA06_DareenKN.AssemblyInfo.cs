4f37ae65cebd7273fe66646df5a7225a41242191
DareenKN <dkinganjatou1@nait.ca>
2026-04-02 10:16:52 -0600

    Completed ICA 8 AND 6

--- icas/ica06/ICA06_DareenKN/ICA06_DareenKN/obj/Debug/net9.0/ICA06_DareenKN.AssemblyInfo.cs
+++ icas/ica06/ICA06_DareenKN/ICA06_DareenKN/obj/Debug/net9.0/ICA06_DareenKN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ICA06_DareenKN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01f3f25068e533104b56c9ccd46dc57890542139")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4f6f587d3f8b9c2e27cf245b666892008c1b786")]
 [assembly: System.Reflection.AssemblyProductAttribute("ICA06_DareenKN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ICA06_DareenKN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
